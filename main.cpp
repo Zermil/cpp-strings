b1fe43a1f688dc355b3fe7e2bb18f956fd2d33e3
Aiden <marcin.madry@interia.com>
2021-12-10 22:52:48 +0100

    Fix double-delete bug, add recursive file parsing

--- main.cpp
+++ main.cpp
@@ -59,7 +59,8 @@ struct global_context {
     
     bool REQ_DISPLAY = false;
     unsigned int LINE_NUMBER = 0;
-    
+
+    bool REQ_RECURSION = false;
     unsigned int MAX_SUBDIRS = 16;
 } context;
 
@@ -82,12 +83,14 @@ size_t get_file_size(const char* filename);
 void parse_and_execute_flag(const char* flag);
 void execute_flag(flag_iterator flag, const std::string& value);
 void execute_flag(flag_iterator flag);
-void throw_error(ERROR_TYPE err, const std::string& flag_name = NULL);
+void throw_error(ERROR_TYPE err, const std::string& flag_name = std::string());
 slurped_strings get_strings_from_file(const slurped_file& slurped_file);
 void parse_file_in_chunks(const char* filename);
 void output_to_file(const slurped_strings& strings, const char* filename);
 void output_based_on_context(const slurped_strings& strings, const char* filename);
 bool file_exists(const char* filename);
+void parse_linear(const char* filename);
+void parse_recursive(const char* filename);
 void usage();
 
 inline bool should_be_added(const std::string& current)
@@ -113,27 +116,17 @@ int main(int argc, char* argv[])
 	usage();
 	exit(1);
     }
-    
-    size_t file_size = get_file_size(argv[1]);
+
     for (int i = 0; i < argc - 2; ++i) {
 	parse_and_execute_flag(argv[i + 2]);
     }
     
-    std::string out = std::string(argv[1]) + "_out.txt";
-    if (context.REQ_OUTPUT && file_exists(out.c_str())) {
-	printf("INFO: Requested output but corresponding '_out.txt' file already exists, it's contents will be replaced.\n");
-	remove(out.c_str());
-    }
-
-    if (file_size > context.MAX_STRINGS_CAP) {
-	parse_file_in_chunks(argv[1]);
+    if (context.REQ_RECURSION) {
+	parse_recursive(argv[1]);
     } else {
-	slurped_file slurped_file = slurp_file_whole(argv[1], file_size);
-	slurped_strings strings = get_strings_from_file(slurped_file);
-    
-	output_based_on_context(strings, argv[1]);
+	parse_linear(argv[1]);
     }
-
+  
     return 0;
 }
 
@@ -168,7 +161,7 @@ slurped_file slurp_file_whole(const char* filename, size_t data_size)
 size_t get_file_size(const char* filename)
 {
     FILE* in = fopen(filename, "rb");
-
+    
     if (in == nullptr) {
 	throw_error(ERROR_TYPE::ERROR_FILE);
 	exit(1);
@@ -263,7 +256,7 @@ void execute_flag(flag_iterator flag)
 	    break;
 
 	case FLAG_TYPE::FLAG_REC:
-	    assert(false && "TODO: File recursion");
+	    context.REQ_RECURSION = true;
 	    break;
 	    
 	default:
@@ -315,12 +308,11 @@ void parse_file_in_chunks(const char* filename)
 	} else {
 	    // Thank you C++ very cool.
 	    printf("\rWritten: %d%% of the file",
-		static_cast<int>((static_cast<double>(++iteration_count * context.MAX_STRINGS_CAP) / full_size) * 100));
+		   static_cast<int>((static_cast<double>(++iteration_count * context.MAX_STRINGS_CAP) / full_size) * 100));
 	}
     }
 
     fclose(file);
-    delete[] buffer;
 }
 
 slurped_strings get_strings_from_file(const slurped_file& slurped_file)
@@ -389,6 +381,54 @@ bool file_exists(const char* filename)
     return false;
 }
 
+void parse_linear(const char* filename)
+{
+    size_t file_size = get_file_size(filename);
+
+    std::string out = std::string(filename) + "_out.txt";
+    if (context.REQ_OUTPUT && file_exists(out.c_str())) {
+	printf("INFO: Requested output but corresponding '_out.txt' file already exists, it's contents will be replaced.\n");
+	remove(out.c_str());
+    }
+    
+    if (file_size > context.MAX_STRINGS_CAP) {
+	parse_file_in_chunks(filename);
+    } else {
+	slurped_file slurped_file = slurp_file_whole(filename, file_size);
+	slurped_strings strings = get_strings_from_file(slurped_file);
+    
+	output_based_on_context(strings, filename);
+    }
+}
+
+void parse_recursive(const char* filename)
+{
+    if (context.MAX_SUBDIRS == 0)
+	return;
+    
+    std::filesystem::path path = std::string(filename);
+    if (path.has_extension()) {
+	path = path.parent_path();
+    }
+
+    for (const auto& entry : std::filesystem::directory_iterator(path)) {
+	const std::string filepath = entry.path().string();
+	const std::string file = entry.path().filename().string();
+
+	if (file[0] == '.')
+	    continue;
+	
+	if (entry.is_directory()) {
+	    if (context.MAX_SUBDIRS > 0)
+		context.MAX_SUBDIRS--;
+
+	    parse_recursive(filepath.c_str());
+	} else {
+	    parse_linear(filepath.c_str());
+	}
+    }
+}
+
 void throw_error(ERROR_TYPE err, const std::string& flag_name)
 {
     switch (err)
@@ -418,15 +458,15 @@ void throw_error(ERROR_TYPE err, const std::string& flag_name)
 	    break;
 
 	case ERROR_TYPE::ERROR_ALLOC:
-	    fprintf(stderr, "ERROR: Could not allocate enough memory.");
+	    fprintf(stderr, "ERROR: Could not allocate enough memory.\n");
 	    break;
 
 	case ERROR_TYPE::ERROR_SIZE:
-	    fprintf(stderr, "ERROR: Provided file's size is not valid. (<= 0)");
+	    fprintf(stderr, "ERROR: Provided file's size is not valid. (<= 0)\n");
 	    break;
 
 	case ERROR_TYPE::ERROR_FILE:
-	    fprintf(stderr, "ERROR: Provided file is invalid or does not exist.");
+	    fprintf(stderr, "ERROR: Provided file is invalid or does not exist.\n");
 	    break;
 	    
 	default:
